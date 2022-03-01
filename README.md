d2cf448314430c4350158659ea1175fa7849126e
Nicolas Perrin <perrin.isir@gmail.com>
2022-03-01 23:08:04 +0100

    init commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # gym-gmazes
 Mazes environments for reinforcement learning (with OpenAI Gym interface)
+
+Install with 'pip install -e .'
---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+# Install with 'pip install -e .'
+
+setup(
+    name="gym_gmazes",
+    version="0.1.0",
+    author="Nicolas Perrin-Gilbert",
+    description="Mazes environments for RL (with OpenAI Gym interface)",
+    url="https://github.com/perrin-isir/gym-gmazes",
+    packages=find_packages(),
+    install_requires=[
+        "gym>=0.22.0",
+        "torch>=1.10.1",
+    ],
+    license="LICENSE",
+)
